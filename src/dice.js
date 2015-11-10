b36946c3033c346dcb24897636908b1d1d8093b4
Lancey6 <david.thomas.reeve@gmail.com>
2015-11-09 19:46:37 -0500

    Added a diceroller module

---
+++ src/dice.js
@@ -0,0 +1,35 @@
+
+function Dice( notation ) {
+  this.notation = notation;
+
+  var spl = notation.split('d');
+  this.count = Number( spl[0] );
+  
+  if ( notation.indexOf( '+' ) > -1 ) {
+    var equ = spl[1].split( '+' );
+    var sides = Number( equ[0] );
+    var constant = Number( equ[1] );
+  } else if ( notation.indexOf( '-' ) > -1 ) {
+    var equ = spl[1].split( '-' );
+    var sides = Number( equ[0] );
+    var constant = -Number( equ[1] )
+  } else {
+    this.sides = spl[1];
+  }
+}
+
+Dice.prototype.rollArray = function() {
+  var results = [];
+  for ( var i = 0; i < this.count; ++i ) {
+    results.push( 1 + Math.floor( Math.random() * this.sides ) );
+  }
+  return results;
+}
+
+Dice.prototype.roll = function() {
+  return this.rollArray().reduce( function( a,b ) {
+    return a + b;
+  }, 0 );
+}
+
+module.exports = Dice;
